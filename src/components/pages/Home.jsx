9460b3053ab4c8eeef60e97e0e23bf8804dd2f15
Alfredo <acorreacen2@gmail.com>
2023-01-17 09:41:24 -0300

    Corrección class

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { Button } from "react-bootstrap";
-import Carrusel from "../carrusel/Carrusel";
+import React, {/*  useState, */ useEffect } from "react";
+/* import { Button } from "react-bootstrap"; */
+/* import Carrusel from "../carrusel/Carrusel"; */
 import Navegacion from "../navegacion/Navegacion";
 import Tarjetas from "../tarjetas/Tarjetas";
 /* import Tabla from "../tabla/Tabla"; */
