137fa1d39ae3f131438f2dae2d548b565d79c565
adolfo85 <adolfodeboeck@gmail.com>
2023-01-20 13:13:11 -0300

    estética footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,15 @@ import { MDBFooter, MDBContainer } from 'mdb-react-ui-kit';
 
 export default function App() {
   return (
-    <MDBFooter className='text-center text-white' style={{ backgroundColor: '#21081a' }}>
-      <MDBContainer className='p-4'></MDBContainer>
+    <MDBFooter className='footer text-center text-black m-4 ' style={{ backgroundColor: '#ffe600' }}>
+      <MDBContainer className='p-4 footer'></MDBContainer>
 
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2020 Copyright:
-        <a className='text-white' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
-        </a>
+      <div className='text-center p-3 footer' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2020 Copyright:Comisión 5I
       </div>
+        <a className='text-black' href='/signup'>
+          E-Commerce 5I
+        </a>
     </MDBFooter>
   );
 }
