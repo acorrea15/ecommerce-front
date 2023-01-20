c9ad6df441bd5430583587aa0ae587eb9a3b77ef
adolfo85 <adolfodeboeck@gmail.com>
2023-01-20 11:30:54 -0300

    función signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -12,6 +12,7 @@ function Signup() {
     const [name, setName] = useState("");
     const [signup, { error, isLoading, isError }] = useSignupMutation();
 
+    // función signup
     function handleSignup(e) {
         e.preventDefault();
         signup({ name, email, password });
@@ -26,17 +27,17 @@ function Signup() {
                         {isError && <Alert variant="danger">{error.data}</Alert>}
                         <Form.Group>
                             <Form.Label>Nombre</Form.Label>
-                            <Form.Control type="text" placeholder="Your name" value={name} required onChange={(e) => setName(e.target.value)} />
+                            <Form.Control type="text" placeholder="Tu nombre" value={name} required onChange={(e) => setName(e.target.value)} />
                         </Form.Group>
 
                         <Form.Group>
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" value={email} required onChange={(e) => setEmail(e.target.value)} />
+                            <Form.Control type="email" placeholder="Email" value={email} required onChange={(e) => setEmail(e.target.value)} />
                         </Form.Group>
 
                         <Form.Group className="mb-3">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Enter Password" value={password} required onChange={(e) => setPassword(e.target.value)} />
+                            <Form.Control type="password" placeholder="Password" value={password} required onChange={(e) => setPassword(e.target.value)} />
                         </Form.Group>
 
                         <Form.Group>
