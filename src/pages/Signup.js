6f5e56d65615f4582a401b5407d30daed936f4c4
adolfo85 <adolfodeboeck@gmail.com>
2023-01-20 11:23:09 -0300

    Comienzo de la signuppage

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import "./Signup.css";
 import { useSignupMutation } from "../services/appApi";
 
+// Estados iniciales de los usuarios: 
+
 function Signup() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
