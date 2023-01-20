43a7dc6dccef6681b8057e94d446b4368d0ca405
adolfo85 <adolfodeboeck@gmail.com>
2023-01-20 14:02:10 -0300

    estética final de footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,18 @@ import { MDBFooter, MDBContainer } from 'mdb-react-ui-kit';
 
 export default function App() {
   return (
-    <MDBFooter className='footer text-center text-black m-4 ' style={{ backgroundColor: '#ffe600' }}>
+    <MDBFooter className='footer m-3 shadow text-center text-black rounded' style={{ backgroundColor: '#ffe600' }}>
       <MDBContainer className='p-4 footer'></MDBContainer>
 
       <div className='text-center p-3 m4 footer' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
         © 2020 Copyright:Comisión 5I
       </div>
-        <a className='text-black p-3 m-4' href='/signup'>
+      <div className='m-3 p-2 text-center'>
+        <a className='texto2 text-black' style={{ textDecoration: 'none' }} href='/signup'>
           E-Commerce 5I
         </a>
+
+      </div>
     </MDBFooter>
   );
 }
