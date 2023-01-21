e06fc9b09bf0030ba994a8cc6eaac09019f45e70
adolfo85 <adolfodeboeck@gmail.com>
2023-01-21 14:50:18 -0300

    cambio de fecha en footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function App() {
       <MDBContainer className='p-4 footer'></MDBContainer>
 
       <div className='text-center p-3 m4 footer' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2020 Copyright:Comisión 5I
+        © 2023 Copyright:Comisión 5I
       </div>
       <div className='m-3 p-2 text-center'>
         <a className='texto2 text-black' style={{ textDecoration: 'none' }} href='/signup'>
