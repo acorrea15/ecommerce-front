359ebdb2dfb77ffce6001faa122e99c65315d5cb
adolfo85 <adolfodeboeck@gmail.com>
2023-01-23 02:37:29 -0300

    logo en el nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,149 +5,173 @@ import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
 import { logout, resetNotifications } from "../features/userSlice";
 import "./Navigation.css";
+import logo from '../imágenes/logodefinitivo.jpg'
 
 function Navigation() {
-    const user = useSelector((state) => state.user);
-    const dispatch = useDispatch();
-    const bellRef = useRef(null);
-    const notificationRef = useRef(null);
-    const [bellPos, setBellPos] = useState({});
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const bellRef = useRef(null);
+  const notificationRef = useRef(null);
+  const [bellPos, setBellPos] = useState({});
 
-    // Función para logout (debe estar en los reducers de los slice!!!
-    // En redux debe asociarse al dispatch
+  // Función para logout (debe estar en los reducers de los slice!!!
+  // En redux debe asociarse al dispatch
 
-    function handleLogout() {
-        dispatch(logout());
-    }
-    const unreadNotifications = user?.notifications?.reduce((acc, current) => {
-        if (current.status == "unread") return acc + 1;
-        return acc;
-    }, 0);
+  function handleLogout() {
+    dispatch(logout());
+  }
+  const unreadNotifications = user?.notifications?.reduce((acc, current) => {
+    if (current.status == "unread") return acc + 1;
+    return acc;
+  }, 0);
 
-    function handleToggleNotifications() {
-        const position = bellRef.current.getBoundingClientRect();
-        setBellPos(position);
-        notificationRef.current.style.display = notificationRef.current.style.display === "block" ? "none" : "block";
-        dispatch(resetNotifications());
-        if (unreadNotifications > 0) axios.post(`/users/${user._id}/updateNotifications`);
-    }
+  function handleToggleNotifications() {
+    const position = bellRef.current.getBoundingClientRect();
+    setBellPos(position);
+    notificationRef.current.style.display =
+      notificationRef.current.style.display === "block" ? "none" : "block";
+    dispatch(resetNotifications());
+    if (unreadNotifications > 0)
+      axios.post(`/users/${user._id}/updateNotifications`);
+  }
 
-    return (
-        <Navbar  expand="lg">
+  return (
+    <Navbar expand="lg">
+      <Container className="containergeneral">
+          <Navbar.Brand href="#home">
+            <img
+              src= {logo}
+              width="100"
+              height="35"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+        <LinkContainer to="/">
 
-            
-            <Container className="containergeneral">
-                <LinkContainer to="/">
+          <Navbar.Brand> ECOMMERCE - 5I </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav.Link href="#features">Sobre Nosotros</Nav.Link>
+          <Nav.Link href="#pricing">Sucursales</Nav.Link>
+          <Nav.Link href="#pricing">Contáctenos</Nav.Link>
 
-                    {/* <Nav.Link> <img src="../src/imágenes/logodefinitivo.jpg" width="30px" height="10px"/>
+          <NavDropdown title="Categorías" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Tecnología</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Teléfonos</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Laptops</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Notebooks</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Otros productos
+            </NavDropdown.Item>
+          </NavDropdown>
 
+          <Nav className="ms-auto">
+            {/* si no hay un usuario registrado mostrar el botón login */}
+            {!user && (
+              <LinkContainer to="/login">
+                <Nav.Link className="text-primary login">Login</Nav.Link>
+              </LinkContainer>
+            )}
 
-                    </Nav.Link> */}
-                    
-                    <Navbar.Brand >  ECOMMERCE - 5I </Navbar.Brand>
-                    
-                    
-                </LinkContainer>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+            {/* Si el usuario está registrado o es admin mostrar cart */}
+            {user && !user.isAdmin && (
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i>
+                  {user?.cart.count > 0 && (
+                    <span className="badge badge-warning" id="cartcount">
+                      {user.cart.count}
+                    </span>
+                  )}
+                </Nav.Link>
+              </LinkContainer>
+            )}
 
-                <Nav.Link   href="#features">Sobre Nosotros</Nav.Link>
-                <Nav.Link  href="#pricing">Sucursales</Nav.Link>
-                <Nav.Link  href="#pricing">Contáctenos</Nav.Link>
+            {/* Si el usuario está registrado: campana, notificaciones etc*/}
+            {user && (
+              <>
+                <Nav.Link
+                  style={{ position: "relative" }}
+                  onClick={handleToggleNotifications}
+                >
+                  <i
+                    className="fas fa-bell"
+                    ref={bellRef}
+                    data-count={unreadNotifications || null}
+                  ></i>
+                </Nav.Link>
+                <NavDropdown title={`${user.email}`} id="basic-nav-dropdown">
+                  {/* Si el usuario es administrador: habilita dashboard y creación de producto  */}
+                  {user.isAdmin && (
+                    <>
+                      <LinkContainer to="/admin">
+                        <NavDropdown.Item>Dashboard</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/new-product">
+                        <NavDropdown.Item>Crear producto</NavDropdown.Item>
+                      </LinkContainer>
+                    </>
+                  )}
+                  {/* Si el usuario no es un administrador:  */}
+                  {!user.isAdmin && (
+                    <>
+                      <LinkContainer to="/cart">
+                        <NavDropdown.Item>Carrito</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/orders">
+                        <NavDropdown.Item>Mis compras</NavDropdown.Item>
+                      </LinkContainer>
+                    </>
+                  )}
 
-                <NavDropdown title="Categorías" id="collasible-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Tecnología</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Teléfonos
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Laptops</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Notebooks</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">
-                        Otros productos
-                    </NavDropdown.Item>
+                  {/* Botón para salir de la sesión (ya sea usuario o admin) con función handlelogout */}
+
+                  <NavDropdown.Divider />
+                  <Button
+                    variant="danger"
+                    onClick={handleLogout}
+                    className="logout-btn"
+                  >
+                    Logout
+                  </Button>
                 </NavDropdown>
-
-                    <Nav className="ms-auto">
-                        {/* si no hay un usuario registrado mostrar el botón login */}
-                        {!user && (
-                            <LinkContainer to="/login" >
-                                <Nav.Link className='text-primary login'>Login</Nav.Link>
-                            </LinkContainer>
-                        )}
-
-                        {/* Si el usuario está registrado o es admin mostrar cart */}
-                        {user && !user.isAdmin && (
-                            <LinkContainer to="/cart">
-                                <Nav.Link>
-                                    <i className="fas fa-shopping-cart"></i>
-                                    {user?.cart.count > 0 && (
-                                        <span className="badge badge-warning" id="cartcount">
-                                            {user.cart.count}
-                                        </span>
-                                    )}
-                                </Nav.Link>
-                            </LinkContainer>
-                        )}
-
-                        {/* Si el usuario está registrado: campana, notificaciones etc*/}
-                        {user && (
-                            <>
-                                <Nav.Link style={{ position: "relative" }} onClick={handleToggleNotifications}>
-                                    <i className="fas fa-bell" ref={bellRef} data-count={unreadNotifications || null}></i>
-                                </Nav.Link>
-                                <NavDropdown title={`${user.email}`} id="basic-nav-dropdown">
-
-                                    {/* Si el usuario es administrador: habilita dashboard y creación de producto  */}
-                                    {user.isAdmin && (
-                                        <>
-                                            <LinkContainer to="/admin">
-                                                <NavDropdown.Item>Dashboard</NavDropdown.Item>
-                                            </LinkContainer>
-                                            <LinkContainer to="/new-product">
-                                                <NavDropdown.Item>Crear producto</NavDropdown.Item>
-                                            </LinkContainer>
-                                        </>
-                                    )}
-                                    {/* Si el usuario no es un administrador:  */}
-                                    {!user.isAdmin && (
-                                        <>
-                                            <LinkContainer to="/cart">
-                                                <NavDropdown.Item>Carrito</NavDropdown.Item>
-                                            </LinkContainer>
-                                            <LinkContainer to="/orders">
-                                                <NavDropdown.Item>Mis compras</NavDropdown.Item>
-                                            </LinkContainer>
-                                        </>
-                                    )}
-
-                                    {/* Botón para salir de la sesión (ya sea usuario o admin) con función handlelogout */}
-
-                                    <NavDropdown.Divider />
-                                    <Button variant="danger" onClick={handleLogout} className="logout-btn">
-                                        Logout
-                                    </Button>
-                                </NavDropdown>
-                            </>
-                        )}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-            {/* notifications */}
-            <div className="notifications-container" ref={notificationRef} style={{ position: "absolute", top: bellPos.top + 30, left: bellPos.left, display: "none" }}>
-                {user?.notifications.length > 0 ? (
-                    user?.notifications.map((notification) => (
-                        <p className={`notification-${notification.status}`}>
-                            {notification.message}
-                            <br />
-                            <span>{notification.time.split("T")[0] + " " + notification.time.split("T")[1]}</span>
-                        </p>
-                    ))
-                ) : (
-                    <p>No notifcations yet</p>
-                )}
-            </div>
-        </Navbar>
-    );
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+      {/* notifications */}
+      <div
+        className="notifications-container"
+        ref={notificationRef}
+        style={{
+          position: "absolute",
+          top: bellPos.top + 30,
+          left: bellPos.left,
+          display: "none",
+        }}
+      >
+        {user?.notifications.length > 0 ? (
+          user?.notifications.map((notification) => (
+            <p className={`notification-${notification.status}`}>
+              {notification.message}
+              <br />
+              <span>
+                {notification.time.split("T")[0] +
+                  " " +
+                  notification.time.split("T")[1]}
+              </span>
+            </p>
+          ))
+        ) : (
+          <p>No notifcations yet</p>
+        )}
+      </div>
+    </Navbar>
+  );
 }
 
 export default Navigation;
