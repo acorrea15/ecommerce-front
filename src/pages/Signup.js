2a8d96e905209034c373f78f3b899b381b66f4d1
adolfo85 <adolfodeboeck@gmail.com>
2023-01-25 13:52:43 -0300

    incorporación del signap a la rama nav2

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -20,30 +20,30 @@ function Signup() {
             <Row>
                 <Col md={6} className="signup__form--container">
                     <Form style={{ width: "100%" }} onSubmit={handleSignup}>
-                        <h1>Create an account</h1>
+                        <h1 className="textotitulo">Crear cuenta</h1>
                         {isError && <Alert variant="danger">{error.data}</Alert>}
                         <Form.Group>
-                            <Form.Label>Name</Form.Label>
-                            <Form.Control type="text" placeholder="Your name" value={name} required onChange={(e) => setName(e.target.value)} />
+                            <Form.Label className="texto">Nombre</Form.Label>
+                            <Form.Control type="text" placeholder="Tu nombre" value={name} required onChange={(e) => setName(e.target.value)} />
                         </Form.Group>
 
                         <Form.Group>
-                            <Form.Label>Email Address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" value={email} required onChange={(e) => setEmail(e.target.value)} />
+                            <Form.Label className="texto">Email</Form.Label>
+                            <Form.Control type="email" placeholder="Email" value={email} required onChange={(e) => setEmail(e.target.value)} />
                         </Form.Group>
 
                         <Form.Group className="mb-3">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Enter Password" value={password} required onChange={(e) => setPassword(e.target.value)} />
+                            <Form.Label className="texto">Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" value={password} required onChange={(e) => setPassword(e.target.value)} />
                         </Form.Group>
 
                         <Form.Group>
-                            <Button type="submit" disabled={isLoading}>
-                                Create account
+                            <Button className="texto" type="submit" disabled={isLoading}>
+                                Crear cuenta
                             </Button>
                         </Form.Group>
-                        <p className="pt-3 text-center">
-                            Don't have an account? <Link to="/login">Login</Link>{" "}
+                        <p className="pt-3 text-center texto">
+                            ¿No tienes cuenta? <Link to="/login">Login</Link>{" "}
                         </p>
                     </Form>
                 </Col>
