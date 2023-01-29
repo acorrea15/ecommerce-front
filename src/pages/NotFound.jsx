709346c06ea4fc834086f7dc873e62a134235c1a
adolfo85 <adolfodeboeck@gmail.com>
2023-01-29 16:13:51 -0300

    fix 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,8 +6,9 @@ export default function NotFound() {
     return (
         <div>
             <h1>Oops! este enlace está fuera de servicio.</h1>
-            <p>Utilizar el siguiente link:<Link to='/'>Home</Link></p>
+{/*             <p>Utilizar el siguiente link:<Link to='/'>Home</Link></p> */}
             
+
 
 
     <Image src="https://colorlib.com/wp/wp-content/uploads/sites/2/404-error-template-3.png" className="img-fluid rounded" alt="error 404"></Image>
