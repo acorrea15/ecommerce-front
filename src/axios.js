7c5ae387e12c5c8122c0342b7a69da2dac1bcb19
Alfredo A. Correa <105443143+acorrea15@users.noreply.github.com>
2023-04-02 20:18:50 -0300

    fix connection

--- src/axios.js
+++ src/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
  
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080",
-    /* baseURL: "https://ecommerce-back-production.up.railway.app/", */
+    /* baseURL: "http://localhost:8080", */
+    baseURL: "https://ecommerce-back-production.up.railway.app/",
     /* headers: {
         Authorization: `Bearer ${accessToken}`
     } */
