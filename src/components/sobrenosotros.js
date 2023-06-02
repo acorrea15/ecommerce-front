3cfc7ffbe5b90756fbb6b93ba47329ddef0babd8
adolfo85 <adolfodeboeck@gmail.com>
2023-06-02 17:15:58 -0300

    paso de los componentes a la carpeta componentes

--- src/components/sobrenosotros.js
+++ src/components/sobrenosotros.js
@@ -9,6 +9,8 @@ import {
   } from 'mdb-react-ui-kit';
 
   import imagenalfredo from '../imágenes/alfredo.jpeg'
+  import imagenadolfo from '../imágenes/adolfo.png'
+
 
 
 function SobreNosotros() {
@@ -22,13 +24,33 @@ function SobreNosotros() {
         </a>
       </MDBRipple>
       <MDBCardBody>
-        <MDBCardTitle>Card title</MDBCardTitle>
+        <MDBCardTitle>Alfredo Correa</MDBCardTitle>
         <MDBCardText>
           Some quick example text to build on the card title and make up the bulk of the card's content.
         </MDBCardText>
       </MDBCardBody>
     </MDBCard>
+
+
+
+    <MDBCard>
+      <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+        <MDBCardImage src={imagenadolfo} fluid alt='...' />
+        <a>
+          <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+        </a>
+      </MDBRipple>
+      <MDBCardBody>
+        <MDBCardTitle>Adolfo De Boeck</MDBCardTitle>
+        <MDBCardText>
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </MDBCardText>
+      </MDBCardBody>
+    </MDBCard>
+
+
       </div>
+
     );
   }
   
